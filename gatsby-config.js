087889e5b5aeb996b1640b814918135a84f37c6d
litianc <719153161@qq.com>
2023-05-12 14:22:56 +0800

    first deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,17 +4,17 @@ module.exports = {
   pathPrefix: process.env.PATH_PREFIX || '/',
   siteMetadata: {
     siteTitle: 'Running Page',
-    siteUrl: 'https://yihong.run',
-    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+    siteUrl: 'https://running.litianc.cn',
+    logo: 'https://encrypted-tbn0.gstatic.com/images?https://litianc.cn/images/avatar.jpg',
     description: 'Personal site and blog',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://yihong.run/running',
+        url: 'https://litianc.cn',
       },
       {
         name: 'About',
-        url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+        url: 'https://github.com/litianc/running_page/blob/master/README-CN.md',
       },
     ],
   },
